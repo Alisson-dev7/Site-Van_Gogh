3962d14e818b6dab0ee7d266183edc130a4a4168
Alisson-dev7 <josealisson977.dev@gmail.com>
2025-06-02 13:50:38 -0300

    0.,

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function sobre(){
     window.location.href="/Sobre.html";
 }
+
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    } else{
+        menuMobile.classList.add('open');
+    }
+}
