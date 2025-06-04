9b77cea6e11817ab4362e8f7ea4afd58b67f8347
Alisson977 <josealisson977.dev@gmail.com>
2025-06-03 22:27:31 -0300

    colocando links

---
+++ produtos/script.js
@@ -0,0 +1,3 @@
+function caderno (){
+  window.location.href="https://produto.mercadolivre.com.br/MLB-2847860205-sketchbook-capa-dura-desenho-e-aquarela-240g-ceu-estrelado-_JM?searchVariation=178413629815&highlight=false&headerTopBrand=false#polycard_client=search-nordic&searchVariation=178413629815&position=44&search_layout=stack&type=item&tracking_id=8dec0039-a696-4506-82c2-4de68d7c5cc8";
+}
