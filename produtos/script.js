f2a98de631840c3a37e1f9b8b961780fa1265548
Alisson-dev7 <josealisson977.dev@gmail.com>
2025-06-04 21:38:00 -0300

    Mudança

--- produtos/script.js
+++ produtos/script.js
@@ -1,3 +1,39 @@
 function caderno (){
   window.location.href="https://produto.mercadolivre.com.br/MLB-2847860205-sketchbook-capa-dura-desenho-e-aquarela-240g-ceu-estrelado-_JM?searchVariation=178413629815&highlight=false&headerTopBrand=false#polycard_client=search-nordic&searchVariation=178413629815&position=44&search_layout=stack&type=item&tracking_id=8dec0039-a696-4506-82c2-4de68d7c5cc8";
 }
+function QuadroM (){
+  window.location.href="https://produto.mercadolivre.com.br/MLB-5371758096-quadro-decorativo-noite-estrelada-van-gogh-moderno-70x50-_JM?searchVariation=183506913496&highlight=false&headerTopBrand=false#polycard_client=search-nordic&searchVariation=183506913496&position=48&search_layout=stack&type=item&tracking_id=c37ecd90-b6d5-41b4-948e-c432abaf4b95";
+}
+function Caneca (){
+  window.location.href="https://produto.mercadolivre.com.br/MLB-1110992735-caneca-pintores-van-gogh-a-noite-estrelada-_JM?highlight=false&headerTopBrand=false#polycard_client=search-nordic&position=37&search_layout=stack&type=item&tracking_id=99b36b32-2c12-4c29-b913-63a892051479&wid=MLB1110992735&sid=search";
+}
+function Quebra (){
+  window.location.href="https://produto.mercadolivre.com.br/MLB-2112381387-quebra-cabeca-2000-pecas-noite-estrelada-s-rodano-van-gogh-_JM#is_advertising=true&backend_model=search-backend&position=17&search_layout=stack&type=pad&tracking_id=9a89ddf5-4509-4f43-8cbe-02e4ae21687f&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=17&ad_click_id=ODEyNjc5N2ItOGJmOS00NzNlLWJmNmItZGE3NTc2ODY4ZWE4";
+}
+function Mousepad(){
+  window.location.href="https://pt.aliexpress.com/item/1005008001973086.html?spm=a2g0o.productlist.main.18.1b2bNN34NN34xM&algo_pvid=e301cb4a-0f91-4d43-a586-6a7b0b4cf096&algo_exp_id=e301cb4a-0f91-4d43-a586-6a7b0b4cf096-17&pdp_ext_f=%7Border%3A78%2Ceval%3A1%7D&pdp_npi=4%40dis%21BRL%2122.74%2122.24%21%21%213.85%213.77%21%402101c5b217490619105544152e8c07%2112000043220025522%21sea%21BR%210%21ABX&curPageLogUid=kEPGW6Yhuuia&utparam-url=scene%3Asearch%7Cquery_from%3A";
+}
+function camiseta (){
+  window.location.href="https://pt.aliexpress.com/item/1005007843025473.html?spm=a2g0o.productlist.main.6.1b2bNN34NN34xM&algo_pvid=c6f94904-6b26-40d6-b1b2-768f49b81d5d&algo_exp_id=c6f94904-6b26-40d6-b1b2-768f49b81d5d-5&pdp_ext_f=%7Border%3A72%2Ceval%3A1%7D&pdp_npi=4%40dis%21BRL%2159.18%2127.31%21%21%2171.96%2133.21%21%402103205217490625838527793e4e54%2112000042465386345%21sea%21BR%210%21ABX&curPageLogUid=ltepDEGsTgmt&utparam-url=scene%3Asearch%7Cquery_from%3A";
+}
+function Pintura (){
+  window.location.href="https://pt.aliexpress.com/item/1005006832372711.html?spm=a2g0o.productlist.main.44.1b2bNN34NN34xM&algo_pvid=e301cb4a-0f91-4d43-a586-6a7b0b4cf096&algo_exp_id=e301cb4a-0f91-4d43-a586-6a7b0b4cf096-41&pdp_ext_f=%7Border%3A458%2Ceval%3A1%7D&pdp_npi=4%40dis%21BRL%2147.89%2116.26%21%21%2158.23%2119.77%21%402101c5b217490619105544152e8c07%2112000038447185409%21sea%21BR%210%21ABX&curPageLogUid=hM7vKsEzV6c2&utparam-url=scene%3Asearch%7Cquery_from%3A";
+}
+function coracao (){
+  window.location.href="https://pt.aliexpress.com/item/4000032414466.html?spm=a2g0o.productlist.main.21.1b2bNN34NN34xM&algo_pvid=e301cb4a-0f91-4d43-a586-6a7b0b4cf096&algo_exp_id=e301cb4a-0f91-4d43-a586-6a7b0b4cf096-20&pdp_ext_f=%7Border%3A27%2Ceval%3A1%7D&pdp_npi=4%40dis%21BRL%2146.26%2127.27%21%21%217.83%214.62%21%402101c5b217490619105544152e8c07%2110000000073278229%21sea%21BR%210%21ABX&curPageLogUid=U1mCsz9bPnT3&utparam-url=scene%3Asearch%7Cquery_from%3A";
+}
+function tapete (){
+  window.location.href="https://pt.aliexpress.com/item/1005008275941539.html?spm=a2g0o.productlist.seoads.4.1b2bNN34NN34xM&p4p_pvid=202506041232423296614400687580000570582_2&_gl=1*11b0sy1*_gcl_aw*R0NMLjE3NDg1Mjk3NTEuQ2p3S0NBandpLURCQmhBNUVpd0FYT0hzR1RGN1B2M2VUbHlaVXlPQV9LY3NPcGVMX2xlWC1BTW84VGU5TW9mUGZFOTQwOGVkMkZwYy14b0NGYU1RQXZEX0J3RQ..*_gcl_dc*R0NMLjE3NDc2Njk3NzQuQ2owS0NRand4SnZCQmhEdUFSSXNBR1VnTmZpUEZuRUh5VEcxNHVlamRGcVZ4ZmR6eTcxNkxjVW41TVhpRmZsaHY1TXdJLVZXT1hqTGVyQWFBamd0RUFMd193Y0I.*_gcl_au*Mzc4NjEwODQyLjE3NDU5MzczNTU.*_ga*MTQ5NzI1OTEzOS4xNzE3MjU1OTAw*_ga_VED1YSGNC7*czE3NDkwNjUzMTYkbzE4JGcxJHQxNzQ5MDY1NTYxJGo2MCRsMCRoMA..&gatewayAdapt=glo2bra";
+}
+function pintura(){
+  window.location.href="https://pt.aliexpress.com/item/1005001864145434.html?spm=a2g0o.productlist.main.12.1b2bNN34NN34xM&algo_pvid=6bf209f9-7330-444e-b298-6dbf727a51d4&algo_exp_id=6bf209f9-7330-444e-b298-6dbf727a51d4-11&pdp_ext_f=%7Border%3A8%2Ceval%3A1%7D&pdp_npi=4%40dis%21BRL%21209.78%21104.89%21%21%2135.51%2117.75%21%402101efeb17490658380104086eb583%2112000017906144897%21sea%21BR%210%21ABX&curPageLogUid=fBN9r6ybHU4n&utparam-url=scene%3Asearch%7Cquery_from%3A";
+}
+
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    } else{
+        menuMobile.classList.add('open');
+    }
+}
